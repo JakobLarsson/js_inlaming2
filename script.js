9f9e53571cecc6c31f34524f75626fe8a3c19fea
JakobLarsson <jakob.larsson@live.se>
2021-02-15 19:35:15 +0100

    Kommentarer på koden

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ submitBtn.addEventListener("click", createWeatherUrl);
 
 submitBtn.addEventListener("click", changeVisibility);
 
-
+//ändra titel till sökt stad
 let title = document.querySelector("#cityHeader");
 function changeTitle(){
   let searchBar = document.querySelector("#inputText");
@@ -20,14 +20,14 @@ function changeTitle(){
   title.innerHTML = nameLocation + ", " + countryLocation;  
 }
 
-
+//ändra titel till error medelande
 function changeTitleOnError(msg){
   title.innerHTML = msg;
   attractionsContent.classList.add("hide");
   weatherContent.classList.add("hide");
 }
 
-
+//skapa en url för att hämta information ifrån openweathermap, parametrar med appid och q för sökning av stad
 let urlWeather = "";
 function createWeatherUrl(){
  let searchBar = document.querySelector("#inputText");
@@ -42,27 +42,35 @@ let searchRes = searchBar.value;
 
 
 //WEATHER API//
+
 let weatherObj = {};
 let temp = 0;
 let tempDescription = "";
 let nameLocation = "";
 let countryLocation = "";
+/*funktion som skapar ett xmlhttprequest objekt för att skicka URL som skapades tidigare. Parametrar som ställs
+in är GET: att jag vill hämta information. Även response type sätts till json*/
   function getWeatherInfo(){
    let xhr = new XMLHttpRequest();
    xhr.open("GET", urlWeather);
    xhr.responeType = "json";
+   //avslutas med ett send kommando
    xhr.send();
    
+   //ONLOAD funktion som körs när hämtningen är klar och hela objektet är nerladdat
    xhr.onload = function() {
-    let responseJson = xhr.response;
+    let responseJson = xhr.response; //spara ner response i en variabel. (som i detta falet är en string av json format)
     
-    weatherObj = JSON.parse(responseJson);
+    weatherObj = JSON.parse(responseJson); //förvandla stringen av json till ett riktigt javascitp objekt
+    /*Om inget felmedelande sker så körs else satsen annars kommer 
+    titlen att ändras till ett error message */
     if(weatherObj.cod != "200"){
       changeTitleOnError(weatherObj.message);
     }else{
       attractionsContent.classList.remove("hide");
   weatherContent.classList.remove("hide");
     }
+    /*Här skickar jag in information från mitt objekt in i enskilda variabler */
     temp = weatherObj.main.temp - 272;
     temp = temp.toFixed(1);
     tempDescription = weatherObj.weather[0].description;
@@ -73,6 +81,8 @@ let countryLocation = "";
     getUrlFour(weatherObj);
 };
 
+/*ONprogress jag lite osäker på hur detta funkar men tror det är ett sätt för att kunna printa ut 
+tillexempel hur långt gången nerladdningen är om man hämtar stora objekt osv, alltså körs denna under pågående nerladdning */
 xhr.onprogress = function(event) {
   if (event.lengthComputable) {
     console.log(`Received ${event.loaded} of ${event.total} bytes`);
@@ -81,6 +91,8 @@ xhr.onprogress = function(event) {
   }
 };
 
+/*ONERROR funktion som körs om något med hämtningen går fel, tex användaren har inget internet
+eller om openweather ligger nere */
 xhr.onerror = function() {
   
   changeTitleOnError("Request failed");
@@ -99,45 +111,50 @@ function updateWeatherDOM(){
 
 
 //GET THE URL FOR FOURSQUARE
-let urlFourSquare = "";
-
+let urlFourSquare = ""; // spara ner slutgiltliga urlen här
+ 
 
 function getUrlFour(obj){
-  let searchBar = document.querySelector("#inputText");
+  let searchBar = document.querySelector("#inputText"); //input text (stad, används inte längre här)
 let searchRes = searchBar.value;
- let urlF = new URL("https://api.foursquare.com/v2/venues/explore");
- urlF.searchParams.append("client_id", "3TQDALDDVBD5JLC1UUUWVUGRKOYFY3COM2Z3YMMWGMAX4W2N");
- urlF.searchParams.append("client_secret", "4QVH4ZCYPYWHB1BX1LTQLITPT41U5MOEKFDPRVX3IDVB11Z1");
- urlF.searchParams.append("ll", obj.coord.lat + "," +  obj.coord.lon);
- urlF.searchParams.append("v", "20210210");
- urlF.searchParams.append("limit", "3");
- urlF.searchParams.append("sortByPopularity", "1");
- urlF.searchParams.append("radius", "3000");
+ let urlF = new URL("https://api.foursquare.com/v2/venues/explore"); //fq standard länk till api
+ urlF.searchParams.append("client_id", "3TQDALDDVBD5JLC1UUUWVUGRKOYFY3COM2Z3YMMWGMAX4W2N"); //implementera client_id til urlen
+ urlF.searchParams.append("client_secret", "4QVH4ZCYPYWHB1BX1LTQLITPT41U5MOEKFDPRVX3IDVB11Z1"); //client secret
+ /*Sök efter ett ställe via kordinater som jag plockar ifrån weather resultatet. Detta för att garantera resultat på samma ställe som weather visar */
+ urlF.searchParams.append("ll", obj.coord.lat + "," +  obj.coord.lon); 
+ urlF.searchParams.append("v", "20210210");//Datum som fq kräver, hämtar inget "dagens datum" vilket jag igentligen borde
+ urlF.searchParams.append("limit", "3"); //Hämta hem endast 3 resultat
+ urlF.searchParams.append("sortByPopularity", "1"); //sort by popularity 1 = true
+ urlF.searchParams.append("radius", "3000"); //sök storlek utifrå¨n kordinaterna
  
- urlFourSquare = urlF.href;
- getFoursQuareInfo();
+ urlFourSquare = urlF.href; //placera den slutgiltliga urlen i variblen skapade tidigare
+ getFoursQuareInfo(); //kör funktionen där ja skickar apit
 }
 
 // FOURSQUARE API
-let answearF = {};
-let answerMain = [answer1 = {name: "", type: "", adress: "", venue: ""},answer2 = {name: "", type: "", adress: "", venue: ""},answer3 = {name: "", type: "", adress: "", venue: ""} ];
+let answearF = {}; //ställe att spara hela slut objektet
+
+//spara ner dom 4 resultaten från varje ställe jag vil åt i en array av objekt.
+let answerMain = [answer1 = {name: "", type: "", adress: "", venue: ""},answer2 = {name: "", type: "", adress: "", venue: ""},answer3 = {name: "", type: "", adress: "", venue: ""} ]; 
+
+
 function getFoursQuareInfo(){
- xhrF = new XMLHttpRequest();
- xhrF.open("GET", urlFourSquare);
- xhrF.onload = function(){
-  let response = xhrF.response;
+ xhrF = new XMLHttpRequest();       //skapa request objekt
+ xhrF.open("GET", urlFourSquare);   //ställ in GET och länka in urlen jag vill skicka
+ xhrF.onload = function(){        
+  let response = xhrF.response;       //onload när hämtningen är färdig sparas det ner i en variabel
   
-  answearF = JSON.parse(xhrF.response);
-  answearF.response.groups[0].items[0].venue.name
-  for(let i = 0; i < answerMain.length; i++){
+  answearF = JSON.parse(xhrF.response); //från json till ett javascript objekt
+  
+  for(let i = 0; i < answerMain.length; i++){ //loopa egenom dom tre venuesen och spara ner dom olika delarna i objektet som skapades tidigare
    answerMain[i].name = answearF.response.groups[0].items[i].venue.name;
    answerMain[i].type = answearF.response.groups[0].items[i].venue.categories[0].name;
    answerMain[i].adress = answearF.response.groups[0].items[i].venue.location.address;
    answerMain[i].venue = answearF.response.groups[0].items[i].venue.id;
   }
   
-  //imgUrlFunc();
-  updateFourSquareDom();
+  imgUrlFunc(); //Kör img url koden
+  updateFourSquareDom(); //uppdatera DOM nu när objektet är uppdaterat med den senaste sökningen
  }
  xhrF.send();
  
@@ -214,9 +231,9 @@ function updateFourSquareDom(){
  c2Type.innerHTML = answerMain[1].type;
  c3Type.innerHTML = answerMain[2].type;
 
- //c1Img.src = picUrl[picUrlCount - 2];
- //c2Img.src = picUrl[picUrlCount - 1];
- //c3Img.src = picUrl[picUrlCount];
+ c1Img.src = picUrl[picUrlCount - 3];
+ c2Img.src = picUrl[picUrlCount - 2];
+ c3Img.src = picUrl[picUrlCount -1];
 
 }
 
